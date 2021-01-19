925bc13c7d0ea1fd74aeae5eec4f0ea693cfce8e
DESKTOP-OSNBSEK\Yellow <hlc>
2021-01-20 00:54:39 +0800

    [MOD] init project

---
+++ app/src/main/java/com/hlc/sxz/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hlc.sxz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
